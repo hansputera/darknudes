6be04fa26c354a5233a3bed43c1565458d01d82d
hansputera <hansputera415@gmail.com>
2020-09-16 21:04:16 +0700

    finally

--- events/message.js
+++ events/message.js
@@ -5,11 +5,10 @@ module.exports = async (bot, message) => {
         bot.sendMessage(message.chat.id, "Bot ini sedang dalam posisi maintenance.");
     } else if (message.chat.type === "private") {
         const msg = message.text.toLowerCase();
-
         if (msg === "/start") {
             bot.sendMessage(message.chat.id, `👋 | Hello, iam ${(await bot.getMe()).username}. My prefix is d!, so you can use d!ping for run command.`);
         }
-        if (msg === bot.getMe().username + "Bot") {
+        if (msg === `@${(await bot.getMe()).first_name.toLowerCase().split(" Bot")[0]}Bot`) {
             return bot.sendMessage(message.chat.id, `👋 | Hello, iam ${(await bot.getMe()).username}.Nice to meet you!`);
         }
 
